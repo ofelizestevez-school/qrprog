3d0e8603f70d85111113aaed57aff856d261f600
ofelizestevez <34465526+ofelizestevez@users.noreply.github.com>
2024-04-21 17:39:53 -0400

    qrcodegenerator pylint fixes

--- qrcodegenerator/__init__.py
+++ qrcodegenerator/__init__.py
@@ -2,19 +2,17 @@
 import qrcode
 
 def create_qr_code_img(data: str):
-	"""Generate QR Code
+    """Generate QR Code
 
 	Args:
 		data (str): data which is being put into a QR code
 	"""
 
-	qr = qrcode.QRCode(
+    qr = qrcode.QRCode(
 		version=1,
 		box_size=10,
 		border=5
 	)
-	qr.add_data(data)
-	qr.make(fit=True)
-	return qr.make_image(fill="black", back_color="white")
-
-create_qr_code_img("test")
+    qr.add_data(data)
+    qr.make(fit=True)
+    return qr.make_image(fill="black", back_color="white")
