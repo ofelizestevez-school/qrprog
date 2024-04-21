a6ade484b8cf51123eb300c3b360b1e3a5110491
ofelizestevez <34465526+ofelizestevez@users.noreply.github.com>
2024-04-21 17:28:27 -0400

    qrcode

---
+++ qrcodegenerator/__init__.py
@@ -0,0 +1,20 @@
+"""Module that creates a qr code from some sort of data."""
+import qrcode
+
+def create_qr_code_img(data: str):
+	"""Generate QR Code
+
+	Args:
+		data (str): data which is being put into a QR code
+	"""
+
+	qr = qrcode.QRCode(
+		version=1,
+		box_size=10,
+		border=5
+	)
+	qr.add_data(data)
+	qr.make(fit=True)
+	return qr.make_image(fill="black", back_color="white")
+
+create_qr_code_img("test")
